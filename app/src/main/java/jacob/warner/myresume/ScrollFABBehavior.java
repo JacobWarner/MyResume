9a2e9702ff7fee7ce7deb73a8c1bdb087a118036
Jacob Warner <jwarner@cyberlaunch.net>
2017-08-07 01:35:00 -0500

    FAB hide/show, spacing, and experience info changes

---
+++ app/src/main/java/jacob/warner/myresume/ScrollFABBehavior.java
@@ -0,0 +1,47 @@
+package jacob.warner.myresume;
+
+import android.content.Context;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by Jacob on 8/6/2017.
+ */
+
+public class ScrollFABBehavior extends FloatingActionButton.Behavior {
+
+    ScrollFABBehavior(){
+        super();
+    }
+
+    ScrollFABBehavior(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
+        if ((dyConsumed > 0 || dyUnconsumed > 0) && child.getVisibility() == View.VISIBLE) {
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onShown(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
+        } else if ((dyConsumed < 0 || dyUnconsumed < 0) && child.getVisibility() == View.INVISIBLE) {
+            child.show();
+        }
+    }
+}
